8fdb2f963d050d4ad6c77a751f6ceb9e4beb4e38
jpc21 <8Ks-sV9-S4f-MQt>
2022-05-26 16:55:41 +0100

    emulator read file

--- src/emulate.c
+++ src/emulate.c
@@ -1,5 +1,42 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+
+#define MEM_SIZE 65536
+#define REGISTERS 17
+
+struct Processor {
+	uint8_t memory[MEM_SIZE];
+	uint32_t registers[REGISTERS];
+};	
+
+uint32_t* load(char filename[]) {
+	FILE *fp;
+	uint64_t size;
+	
+	fp = fopen(filename, "rb");
+	fseek(fp, 0, SEEK_END);
+	uint64_t filesize = ftell(fp);
+	size = filesize / 4 + (filesize % 4 != 0);
+	fseek(fp, 0, SEEK_SET);
+
+	uint32_t* instructions = (uint32_t *) malloc(size);
+	fread(instructions, 4, size, fp); 	
+	fclose(fp);
+	for (int i = 0; i < size; i++) {
+		printf("%d", instructions[i]);
+	}
+
+	return instructions;
+}
 
 int main(int argc, char **argv) {
-  return EXIT_SUCCESS;
+	if (argc == 2) {
+		printf("%s\n", argv[1]);
+	} else if (argc > 2) {
+		printf("Too many arguments supplied\n");
+	} else {
+		printf("No arguments\n");
+	}
+	return EXIT_SUCCESS;
 }
