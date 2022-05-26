f0e44776097c492d92a08e7f11706c64c1071ea7
jpc21 <8Ks-sV9-S4f-MQt>
2022-05-26 18:19:32 +0100

    8 bit loading attempt

--- src/emulate.c
+++ src/emulate.c
@@ -10,24 +10,34 @@ struct Processor {
 	uint32_t registers[REGISTERS];
 };	
 
-uint32_t* load(char filename[]) {
+uint8_t* load(char filename[]) {
 	FILE *fp;
 	uint64_t size;
 	
 	fp = fopen(filename, "rb");
 	fseek(fp, 0, SEEK_END);
 	uint64_t filesize = ftell(fp);
-	size = filesize / 4 + (filesize % 4 != 0);
+	size = filesize / 4;
 	fseek(fp, 0, SEEK_SET);
 
-	uint32_t* instructions = (uint32_t *) malloc(size);
-	fread(instructions, 4, size, fp); 	
+	uint8_t* instructions = (uint8_t *) malloc(filesize);
+	fread(instructions, 1, filesize, fp); 	
 	fclose(fp);
 	printf("%ld\n", size);
-	for (int i = 0; i < size; i++) {
+	
+	for (int i = 0; i < filesize; i++) {
+		for (int j = 7; j >= 0; j--) {
+			uint8_t k;
+			k = (instructions[i] & (1 << j)) >> j;
+			printf("%d", k);
+		}
+		printf("\n");
+	}
+	
+
+	for (int i = 0; i < filesize; i++) {
 		printf("%u\n", instructions[i]);
 	}
-
 	return instructions;
 }
 
