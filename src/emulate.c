e8cd22c11b2e4b0010e0eb7ff8a1094bb25d20e6
Nada Struharova <ns2321@ic.ac.uk>
2022-05-31 19:27:43 +0100

    Fix tabs

--- src/emulate.c
+++ src/emulate.c
@@ -70,11 +70,11 @@ bool condition_check(uint32_t type) {
 //return true: clear pipeline
 //return false: leave pipeline intact
 bool process_instructions(uint8_t* instruction_bytes) {
-	uint32_t *instruction = realloc(instruction_bytes, BITS_PER_INSTRUCTION);
+uint32_t *instruction = realloc(instruction_bytes, BITS_PER_INSTRUCTION);
     uint32_t first4bits = create_mask(31, 28, instruction);
     static const uint8_t PIPELINE_CORRECTION = 8;
     switch (get_instr_type(instruction)) {
-		case BRANCH :
+	case BRANCH :
             if (condition_check(first4bits)) {
                 int32_t offset = (int32_t) (create_mask(0, 23, instruction)) << 2;
                 if (offset < 0) {
@@ -84,13 +84,13 @@ bool process_instructions(uint8_t* instruction_bytes) {
 				return true;
             }
 			return false;
-		case TRANSFER :
+	case TRANSFER :
 			execute_single_data_transfer(processor, instruction);
-		case MULTIPLY :
+	case MULTIPLY :
 			execute_multiply_instruction(processor, instruction);
-		case DATA_PROCESS :
+	case DATA_PROCESS :
 			execute_data_processing_instruction(processor, instruction);
-		default:
+	default:
 			exit(EXIT_FAILURE);
 	}		
 }
