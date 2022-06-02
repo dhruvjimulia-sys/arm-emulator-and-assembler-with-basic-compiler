af62c93d0c8cc3a0ee63d177f411bf039c447a06
Nada Struharova <ns2321@ic.ac.uk>
2022-06-02 21:22:00 +0100

    fix pointers in symbol_table, symbol_table now compiles

--- src/symbol_table.h
+++ src/symbol_table.h
@@ -5,32 +5,32 @@
 #ifndef SYMBOL_TABLE_H
 #define SYMBOL_TABLE_H
 
-typedef struct {
-    char *symbol;
-    uint32_t address;
-    struct entry *next_entry;
+typedef struct hash_table_entry {
+	char *symbol;
+	uint32_t address;
+	struct hash_table_entry *next;
 } entry;
 
 typedef struct {
-    entry **entries;  
-    size_t count;   //number of entries already in the hash table
-    size_t size;
+	entry **entries;  
+	unsigned int count;   //number of entries already in the hash table
+	unsigned int size;
 } hash_table;
 
 void free_hash_table(hash_table *symbol_table);
 
 hash_table *create_hash_table(void);
 
-unsigned int hash(char *s, hash_table *symbol_table);
+unsigned int hash(char *s, unsigned int size_of_table);
 
-void create_entry(char *s, uint32_t address);
+entry *create_entry(char *s, uint32_t address);
 
 bool resize(hash_table *symbol_table);
 
-void insert(char *s, uint32_t address, entry *entries);
+void insert(char *s, uint32_t address, entry **entries, unsigned int size_of_table, hash_table *symbol_table);
 
 uint32_t lookup(char *s, hash_table *symbol_table);
 
-void rehash(entry *rehash_entries);
+void rehash(hash_table *symbol_table, entry **rehash_entries, unsigned int new_size);
 
 #endif
