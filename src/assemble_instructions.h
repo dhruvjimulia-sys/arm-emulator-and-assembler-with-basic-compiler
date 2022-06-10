624185d0e9c1ab1c4b73ed0b3526e7fc3bc045f4
Nada Struharova <ns2321@ic.ac.uk>
2022-06-10 14:59:38 +0100

    fix header files, include guards

--- src/assemble_instructions.h
+++ src/assemble_instructions.h
@@ -12,10 +12,10 @@ uint32_t assemble_data_processing(TokenizedInstruction *token_instr);
 uint32_t assemble_multiply(TokenizedInstruction *token_instr);
 
 //assemble a single data transfer instruction
-uint32_t assemble_single_data_transfer(TokenizedInstruction *token_instr, uint32_t data_address, uint32_t total_of_instr);
+uint32_t assemble_single_data_transfer(TokenizedInstruction *token_instr, uint32_t pc_address, uint32_t total_of_instr);
 
 //assemble a branch instruction
-uint32_t assemble_branch(TokenizedInstruction *token_instr, hash_table *symbol_table, uint32_t data_address);
+uint32_t assemble_branch(TokenizedInstruction *token_instr, uint32_t pc_address);
 
 //set operand2 field or offset field to the value of the <#expression>
 uint32_t set_expression(char *operand_as_str, uint32_t assembled_instr);
